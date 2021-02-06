ef6664d2b4b9d7bd1f5b90a7013fedaccbfd700b
Arho Huttunen <arho.huttunen@iki.fi>
2021-02-06 19:39:04 +0200

    Add support for DynamoDB Streams client resolving

--- dynamodb/src/main/kotlin/com/github/awstest/dynamodb/junit5/DynamoDbClientParameterResolver.kt
+++ dynamodb/src/main/kotlin/com/github/awstest/dynamodb/junit5/DynamoDbClientParameterResolver.kt
@@ -7,6 +7,10 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClientBuilder
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsAsyncClient
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsAsyncClientBuilder
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsClient
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsClientBuilder
 import kotlin.reflect.KClass
 
 class DynamoDbClientParameterResolver : SdkClientParameterResolver() {
@@ -15,7 +19,15 @@ class DynamoDbClientParameterResolver : SdkClientParameterResolver() {
     init {
         factories = mapOf(
             DynamoDbClient::class to AwsClientFactory<DynamoDbClientBuilder, DynamoDbClient>(DynamoDbClient.builder()),
-            DynamoDbAsyncClient::class to AwsClientFactory<DynamoDbAsyncClientBuilder, DynamoDbAsyncClient>(DynamoDbAsyncClient.builder())
+            DynamoDbAsyncClient::class to AwsClientFactory<DynamoDbAsyncClientBuilder, DynamoDbAsyncClient>(
+                DynamoDbAsyncClient.builder()
+            ),
+            DynamoDbStreamsClient::class to AwsClientFactory<DynamoDbStreamsClientBuilder, DynamoDbStreamsClient>(
+                DynamoDbStreamsClient.builder()
+            ),
+            DynamoDbStreamsAsyncClient::class to AwsClientFactory<DynamoDbStreamsAsyncClientBuilder, DynamoDbStreamsAsyncClient>(
+                DynamoDbStreamsAsyncClient.builder()
+            )
         )
     }
 }
--- dynamodb/src/test/kotlin/com/github/awstest/dynamodb/junit5/DynamoDbClientParameterResolverTest.kt
+++ dynamodb/src/test/kotlin/com/github/awstest/dynamodb/junit5/DynamoDbClientParameterResolverTest.kt
@@ -5,16 +5,28 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsAsyncClient
+import software.amazon.awssdk.services.dynamodb.streams.DynamoDbStreamsClient
 
 @ExtendWith(DynamoDbClientParameterResolver::class)
 class DynamoDbClientParameterResolverTest {
     @Test
-    fun `resolve DynamoDB client as an argument`(@AwsClient dynamoDbClient: DynamoDbClient) {
-        assertThat(dynamoDbClient).isNotNull()
+    fun `resolve DynamoDB client as an argument`(@AwsClient client: DynamoDbClient) {
+        assertThat(client).isNotNull()
     }
 
     @Test
-    fun `resolve DynamoDB async client as an argument`(@AwsClient dynamoDbAsyncClient: DynamoDbClient) {
-        assertThat(dynamoDbAsyncClient).isNotNull()
+    fun `resolve DynamoDB async client as an argument`(@AwsClient client: DynamoDbClient) {
+        assertThat(client).isNotNull()
+    }
+
+    @Test
+    fun `resolve DynamoDB Streams client as an argument`(@AwsClient client: DynamoDbStreamsClient) {
+        assertThat(client).isNotNull()
+    }
+
+    @Test
+    fun `resolve DynamoDB Streams async client as an argument`(@AwsClient client: DynamoDbStreamsAsyncClient) {
+        assertThat(client).isNotNull()
     }
 }
