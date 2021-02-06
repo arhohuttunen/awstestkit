19d5540ab16657e227e466cca2750244afa31040
Arho Huttunen <arho.huttunen@iki.fi>
2021-02-06 19:28:42 +0200

    Add simple Kinesis support

---
+++ kinesis/src/main/kotlin/com/github/awstest/kinesis/junit5/KinesisClientParameterResolver.kt
@@ -0,0 +1,21 @@
+package com.github.awstest.kinesis.junit5
+
+import com.github.awstest.AwsClientFactory
+import com.github.awstest.SdkClientParameterResolver
+import software.amazon.awssdk.core.SdkClient
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClientBuilder
+import software.amazon.awssdk.services.kinesis.KinesisClient
+import software.amazon.awssdk.services.kinesis.KinesisClientBuilder
+import kotlin.reflect.KClass
+
+class KinesisClientParameterResolver : SdkClientParameterResolver() {
+    override val factories: Map<KClass<out SdkClient>, AwsClientFactory<*, *>>
+
+    init {
+        factories = mapOf(
+            KinesisClient::class to AwsClientFactory<KinesisClientBuilder, KinesisClient>(KinesisClient.builder()),
+            KinesisAsyncClient::class to AwsClientFactory<KinesisAsyncClientBuilder, KinesisAsyncClient>(KinesisAsyncClient.builder())
+        )
+    }
+}
