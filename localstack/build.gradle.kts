6b9f76478e278e76f58b46d5a028c4dc225c1e80
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2025-12-05 17:25:45 +0200

    Upgrade to kotest 6

--- localstack/build.gradle.kts
+++ localstack/build.gradle.kts
@@ -20,6 +20,6 @@ dependencies {
     implementation("com.amazonaws:aws-java-sdk-core:1.12.792")
     implementation("org.testcontainers:localstack:1.21.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:6.0.7")
     testImplementation("ch.qos.logback:logback-classic:1.5.18")
 }
